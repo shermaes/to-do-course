819baeb81774aad3e1082783b770d224424bac40
Sherilyn Maestre <you@example.com>
2022-08-04 18:42:29 -0500

    working on submitting tasks

--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -1,32 +1,53 @@
 import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
-import {useState} from 'react'
+import { useState } from "react";
+
 const options = [
-    {key: "deporte", text:"Deporte", value: "deporte" },
-    {key: "casa", text:" Casa", value: "casa" },
-    {key: "oficina", text:"Oficina", value: "oficina" },
-    {key: "otra", text:"Otra", value: "otra" },
-]
+  { key: "deporte", text: "Deporte", value: "deporte" },
+  { key: "casa", text: " Casa", value: "casa" },
+  { key: "oficina", text: "Oficina", value: "oficina" },
+  { key: "otra", text: "Otra", value: "otra" },
+];
+
 export default function InputTask() {
-    //el siguiente useState va a tener un objeto con todos los atributos que necesitare para manejar mis tareas
-    const [task, setTask] = useState({
-        idTask: "",
-        taskName:"",
-        categoryName:""
-    })
-    //dentro del formulario asigno todos los valores que recolectare para actualizar mi estado
+  //el siguiente useState va a tener un objeto con todos los atributos que necesitare para manejar mis tareas
+  const [task, setTask] = useState({
+    idTask: "",
+    taskName: "",
+    categoryTask: "",
+  });
+  //dentro del formulario asigno todos los valores que recolectare para actualizar mi estado
 
-    const [error, setError] = useState(false);
+  const [error, setError] = useState(false);
 
-    const onChangeTask = (e) => {
-     console.log(e.target.value);
-     setTask({
-       //lo que hacemos con los ... es que le decimos que lo que estamos recolectando lo vamos a mantener ahi hasta que terminemos
-        ...task,
-        // aca estamos actualizando taskname con el valo que recibimos
-        [e.target.name]: e.target.value,
-     });
-    };
-    return (
+
+//recuperando datos del input
+  const onChangeTask = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+
+//recuperando datos del selector
+  const onChangeCategoryTask = (e, data) => {
+    setTask({...task, [data.name]: data.value });
+  };
+  
+ //creando la funcion para el submit
+ const onSubmitTask = () => {
+   // que no recarge la pagina
+
+   //validacion
+
+   //eliminar el mensaje previo 
+
+   //asignar un ID
+
+   //crear la tarea 
+
+   //limpiar los inputs
+ } 
+
+
+  return (
     <>
       <Grid centered columns={2}>
         <Input type="text" action>
@@ -35,7 +56,7 @@ export default function InputTask() {
             icon="add"
             placeholder="Escribe tu tarea..."
             iconPosition="left"
-            name="taskname"
+            name="taskName"
             value={task.taskName}
             onChange={onChangeTask}
           />
@@ -45,18 +66,21 @@ export default function InputTask() {
             className="select-form-task"
             name="categoryTask"
             placeholder="Categoria"
-            value={task.categoryName}
+            value={task.categoryTask}
+            onChange={onChangeCategoryTask}
           />
-          <Button type="submit" color="teal">Añadir tarea</Button>
+          <Button type="submit" color="teal" onClick={onSubmitTask}>
+            Añadir tarea
+          </Button>
         </Input>
       </Grid>
       {error && (
         <Grid centered>
-          <Header as="h4" color="red" className="alert-error-form" >
-            <Icon name="close"/>
+          <Header as="h4" color="red" className="alert-error-form">
+            <Icon name="close" />
             <Header.Content>La tarea es obligatoria</Header.Content>
-            <Icon name="close"/>
-            </Header> 
+            <Icon name="close" />
+          </Header>
         </Grid>
       )}
     </>
