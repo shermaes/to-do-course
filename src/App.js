6875b72be8e7fdc26ea89b60d43b072660a7f0e8
Sherilyn Maestre <you@example.com>
2022-08-05 11:11:21 -0500

    saving the tasks in local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useState, useEffect} from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import Container from './components/Container';
 import Header from './components/Header';
 import InputTask from './components/InputTask';
 
 function App() {
-  //esta funcion se la pasaremos por props a Input task para poder crear las tasks
-  const createTask = (task ) => {
-    console.log(task);
-  }
+   // pasar las tareas a localstorage
+   let initialTasks = JSON.parse(localStorage.getItem("tasks"));
+
+   if (!initialTasks) {
+     initialTasks = [];
+   }
+ 
+   const [tasks, setTasks] = useState(initialTasks);
+ 
+   //guardando las tasks en el localstorage
+   useEffect(() => {
+     if (initialTasks) {
+      //actualizamos el value de "tasks" con lo wue tengo en tasks
+       localStorage.setItem("tasks", JSON.stringify(tasks));
+     } else {
+       localStorage.setItem("tasks", JSON.stringify([]));
+     }
+   }, [initialTasks, tasks]);
+ 
+   const createTask = (task) => {
+     setTasks([...tasks, task]);
+   };
+ 
+   const deleteTask = (id) => {
+     const currentTask = tasks.filter((task) => task.idTask !== id);
+     setTasks(currentTask);
+   };
+ 
+  console.log(tasks);
+
   return (
   <Container>
     <Header/>
